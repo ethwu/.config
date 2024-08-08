20991cafe7d7adf23928c8557c7ec920c01f0db9
Ethan Wu <ethan.wu@ibm.com>
2024-08-08 11:33:27 -0700

    Improve diagnostics

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -8,7 +8,7 @@ local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
     local win_width = vim.fn.winwidth(0)
     if hide_width and win_width < hide_width then return ''
     elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
-       return str:sub(1, trunc_len) .. (no_ellipsis and '' or '...')
+       return str:sub(1, trunc_len) .. (no_ellipsis and '' or '…')
     end
     return str
   end
@@ -17,17 +17,13 @@ end
 
 local theme = {
 	normal = {
-		a = { fg = 15, bg = 13, gui = 'italic' },
+		a = { fg = 15, bg = 4, gui = 'italic' },
 		b = { fg = 15, bg = 3 },
 		c = { gui = 'italic' },
 		y = { fg = 15, bg = 7 },
 		z = { fg = 15, bg = 4 },
 	}
 }
-
-local function window()
-	return vim.api.nvim_win_get_number(0)
-end
 
 local colors = {
 	black = 0,
@@ -86,7 +82,9 @@ local function mode_color()
 		gui = 'italic',
 	}
 end
+local inactive_color = { bg = colors.magenta + bright, gui = 'italic' }
 
+local inactive = { "'inactive'", color = inactive_color }
 local mode = { 'mode', fmt = string.lower, color = mode_color }
 local branch = { 'branch', icons_enabled = true, icon = '', fmt = trunc(60, 60, 60, false), separator = '' }
 local diff = { 'diff', diff_color = {
@@ -94,6 +92,12 @@ local diff = { 'diff', diff_color = {
 	modified = { fg = colors.orange + bright, gui = 'bold' },
 	removed = { fg = colors.red + bright, gui = 'bold' },
 } }
+local diagnostics = { 'diagnostics', diagnostics_color = {
+	error = { fg = colors.red },
+	warn = { fg = colors.yellow },
+	info = { fg = colors.blue },
+	hint = { fg = colors.orange },
+}, update_in_insert = true, fmt = trunc(80, 80, 80, false) }
 local filename = { 'filename', newfile_status = true, separator = '', symbols = {
 	modified = '[+]', readonly = '[-]', unnamed = '[No Name]', newfile = '[New]',
 } }
@@ -113,9 +117,10 @@ local filesize = {
 	padding = { left = 0, right = 1 },
 	color = { gui = 'None' },
 }
-local lineno = { function() return vim.fn.line'.' end, icons_enabled = true, icon = '', separator = '', padding = { left = 1, right = 0 } }
+local lineno = { function() return vim.fn.line'.' end, icons_enabled = true, icon = '', } -- separator = '', padding = { left = 1, right = 0 } }
 local colno = { function() return vim.fn.charcol'.' end, icons_enabled = true, icon = '' }
 -- local readonly = { "''", cond = function() return vim.bo[vim.api.nvim_win_get_buf(0)].readonly end }
+local windowno = { "vim.api.nvim_win_get_number(0)", color = { bg = colors.magenta }, cond = function() return #vim.api.nvim_list_wins() > 1 end }
 
 return {
 	'nvim-lualine/lualine.nvim',
@@ -137,36 +142,34 @@ return {
 				filename,
 				filesize,
 				diff,
-				{ 'diagnostics', fmt = trunc(80, 80, 80, false) },
+				diagnostics,
 			},
 			lualine_x = {
-				{ 'encoding', fmt = trunc(80, 80, 80, false) },
-				{ 'fileformat', fmt = trunc(80, 80, 80, false) },
-				{ 'filetype', fmt = trunc(60, 60, 60, false)  },
+				{ 'encoding', fmt = trunc(100, 100, 100, false) },
+				{ 'fileformat', fmt = trunc(100, 100, 100, false) },
+				{ 'filetype', fmt = trunc(80, 80, 80, false)  },
 			},
-			lualine_y = { { 'progress', fmt = trunc(60, 60, 60, false) } },
+			lualine_y = { { 'progress', fmt = trunc(80, 80, 80, false) } },
 			lualine_z = {
 				-- { location, icons_enabled = true, icon = '' },
 				lineno,
 				colno,
-				{
-					window,
-					color = { bg = colors.magenta },
-					cond = function() return #vim.api.nvim_list_wins() > 1 end,
-				},
+				windowno,
 			},
 		},
 		inactive_sections = {
-			lualine_a = { "'inactive'" },
+			lualine_a = { inactive },
 			lualine_b = { },
-			lualine_c = { filename, filesize, diff },
+			lualine_c = { filename, filesize, diff, diagnostics },
 			lualine_x = {},
 			lualine_y = { },
 			lualine_z = {
 				lineno, colno,
-				{ window, color = { bg = colors.magenta } },
+				windowno,
 			},
 		},
+		tabline = {},
+		winbar = {},
 		extensions = {
 			'fugitive',
 			'mason',
