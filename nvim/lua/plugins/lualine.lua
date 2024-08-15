515f0e0c23df83aa8fece13625267969f8ed94bf
Ethan Wu <ethan.wu@ibm.com>
2024-08-14 17:55:05 -0700

    New progress bar

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,14 +4,15 @@
 --- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
 --- return function that can format the component accordingly
 local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
-  return function(str)
-    local win_width = vim.fn.winwidth(0)
-    if hide_width and win_width < hide_width then return ''
-    elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
-       return str:sub(1, trunc_len) .. (no_ellipsis and '' or '…')
-    end
-    return str
-  end
+	return function(str)
+		local win_width = vim.fn.winwidth(0)
+		if hide_width and win_width < hide_width then
+			return ''
+		elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+			return str:sub(1, trunc_len) .. (no_ellipsis and '' or '…')
+		end
+		return str
+	end
 end
 
 
@@ -87,27 +88,40 @@ local inactive_color = { bg = colors.magenta + bright, gui = 'italic' }
 local inactive = { "'inactive'", color = inactive_color }
 local mode = { 'mode', fmt = string.lower, color = mode_color }
 local branch = { 'branch', icons_enabled = true, icon = '', fmt = trunc(60, 60, 60, false), separator = '' }
-local diff = { 'diff', diff_color = {
-	added = { fg = colors.green + bright, gui = 'bold' },
-	modified = { fg = colors.orange + bright, gui = 'bold' },
-	removed = { fg = colors.red + bright, gui = 'bold' },
-} }
-local diagnostics = { 'diagnostics', diagnostics_color = {
-	error = { fg = colors.red },
-	warn = { fg = colors.yellow },
-	info = { fg = colors.blue },
-	hint = { fg = colors.orange },
-}, update_in_insert = true, fmt = trunc(80, 80, 80, false) }
-local filename = { 'filename', newfile_status = true, separator = '', symbols = {
-	modified = '[+]', readonly = '[-]', unnamed = '[No Name]', newfile = '[New]',
-} }
+local diff = {
+	'diff',
+	diff_color = {
+		added = { fg = colors.green + bright, gui = 'bold' },
+		modified = { fg = colors.orange + bright, gui = 'bold' },
+		removed = { fg = colors.red + bright, gui = 'bold' },
+	}
+}
+local diagnostics = {
+	'diagnostics',
+	diagnostics_color = {
+		error = { fg = colors.red },
+		warn = { fg = colors.yellow },
+		info = { fg = colors.blue },
+		hint = { fg = colors.orange },
+	},
+	update_in_insert = true,
+	fmt = trunc(80, 80, 80, false)
+}
+local filename = {
+	'filename',
+	newfile_status = true,
+	separator = '',
+	symbols = {
+		modified = '[+]', readonly = '[-]', unnamed = '[No Name]', newfile = '[New]',
+	}
+}
 local fullfilename = filename
 fullfilename['path'] = 3
 local filesize = {
 	'filesize',
 	fmt = function(s)
 		if s ~= '' then
-			if s:match'b$' then
+			if s:match 'b$' then
 				return '[' .. s:upper() .. ']'
 			else
 				return '[' .. s:upper() .. 'iB]'
@@ -119,31 +133,38 @@ local filesize = {
 	padding = { left = 0, right = 1 },
 	color = { gui = 'None' },
 }
-local lineno = { function() return vim.fn.line'.' end, icons_enabled = true, icon = '', } -- separator = '', padding = { left = 1, right = 0 } }
-local colno = { function() return vim.fn.charcol'.' end, icons_enabled = true, icon = '' }
--- local readonly = { "''", cond = function() return vim.bo[vim.api.nvim_win_get_buf(0)].readonly end }
-local windowno = { "vim.api.nvim_win_get_number(0)", color = { bg = colors.magenta }, cond = function() return #vim.api.nvim_list_wins() > 1 end }
--- this can probably be genericized to an arbitrary width, but I just
--- really don't want to deal with that rn
-local progress_indicators = {
-	-- '⠁⠀', '⠃⠀', '⠇⠀', '⡇⠀', '⡏⠀', '⡟⠀', '⡿⠀', '⣿⠀',
-	-- '⣿⠁', '⣿⠃', '⣿⠇', '⣿⡇', '⣿⡏', '⣿⡟', '⣿⡿',
-	'▏   ', '▎   ', '▍   ', '▌   ', '▋   ', '▊   ', '▉   ', '█   ',
-	'█▏  ', '█▎  ', '█▍  ', '█▌  ', '█▋  ', '█▊  ', '█▉  ', '██  ',
-	'██▏ ', '██▎ ', '██▍ ', '██▌ ', '██▋ ', '██▊ ', '██▉ ', '███ ',
-	'███▏', '███▎', '███▍', '███▌', '███▋', '███▊', '███▉',
+local lineno = { function() return vim.fn.line '.' end, icons_enabled = true, icon = '', } -- separator = '', padding = { left = 1, right = 0 } }
+local colno = { function() return vim.fn.charcol '.' end, icons_enabled = true, icon = '' }
+local readonly = { "''", cond = function() return vim.bo[vim.api.nvim_win_get_buf(0)].readonly end }
+local windowno = { "vim.api.nvim_win_get_number(0)", color = { bg = colors.magenta }, cond = function() return #vim.api
+	.nvim_list_wins() > 1 end }
+-- improved progress impl based on
+-- https://github.com/verigak/progress/blob/52de04b95db27fc7db32f81b9fd0887e32af9d00/progress/bar.py#L71
+local progress_width = 4
+local progress_indicator_styles = {
+	braille = { '⠁', '⠃', '⠇', '⡇', '⡏', '⡟', '⡿', '⣿', },
+	shading = { '░', '▒', '▓', '█', },
+	block = { '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█', },
 }
--- local progress_width = 3
+local progress_indicators = progress_indicator_styles['block']
 local function progress()
 	local cur = vim.fn.line('.')
 	local total = vim.fn.line('$')
-	if cur == 1 then
-		return '    '
+	if total == 1 then
+		return ('🮕'):rep(progress_width)
+	elseif cur == 1 then
+		return ('▔'):rep(progress_width)
 	elseif cur == total then
-		return '████'
-	else
-		return progress_indicators[math.floor(cur * #progress_indicators / total) + 1]
+		return ('█'):rep(progress_width)
 	end
+	local prog = progress_width * cur / total
+	local full = math.floor(prog)
+	local phase = math.floor((prog - full) * #progress_indicators)
+	local filled = progress_indicators[#progress_indicators]:rep(full)
+	-- local current if phase > 0 then current = progress_indicators[phase] else current = '' end
+	local current if phase > 0 then current = progress_indicators[phase] else current = ' ' end
+	local empty = (' '):rep(math.max(progress_width - full - 1, 0))
+	return filled .. current .. empty
 end
 
 return {
@@ -169,9 +190,9 @@ return {
 				diagnostics,
 			},
 			lualine_x = {
-				{ 'encoding', fmt = trunc(100, 100, 100, false) },
+				{ 'encoding',   fmt = trunc(100, 100, 100, false) },
 				{ 'fileformat', fmt = trunc(100, 100, 100, false) },
-				{ 'filetype', fmt = trunc(80, 80, 80, false) },
+				{ 'filetype',   fmt = trunc(80, 80, 80, false) },
 			},
 			lualine_y = { { progress, padding = 0, fmt = trunc(80, 80, 80, false) } },
 			lualine_z = {
@@ -183,7 +204,7 @@ return {
 		},
 		inactive_sections = {
 			lualine_a = { inactive },
-			lualine_b = { },
+			lualine_b = {},
 			lualine_c = { filename, filesize, diff, diagnostics },
 			lualine_x = {},
 			lualine_y = { { progress, padding = 0, fmt = trunc(80, 80, 80, false) } },
@@ -193,13 +214,14 @@ return {
 				windowno,
 			},
 		},
-		tabline = {
-			lualine_a = {
-				{ 'buffers', mode = 2, symbols = { modified = '[+]', directory = '🮹🮺' } },
-			},
-			lualine_c = { fullfilename },
-			lualine_z = { 'tabs' },
-		},
+		-- tabline = {
+		-- 	lualine_a = {
+		-- 		{ 'buffers', mode = 2, symbols = { modified = '[+]', directory = '🮹🮺' } },
+		-- 	},
+		-- 	lualine_b = { readonly },
+		-- 	lualine_c = { fullfilename },
+		-- 	lualine_z = { 'tabs' },
+		-- },
 		winbar = {},
 		extensions = {
 			'fugitive',
